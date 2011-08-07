1867c513336372a110033228554a396234a8516f
Aaron Brethorst <aaron@Monolith.local>
2011-08-07 12:41:30 -0700

    Initial Commit

---
+++ Orca/OrcaAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  OrcaAppDelegate.h
+//  Orca
+//
+//  Created by Aaron Brethorst on 8/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrcaAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ Orca/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Orca
+//
+//  Created by Aaron Brethorst on 8/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
