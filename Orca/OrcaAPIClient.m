1f2d43b4653f1953cf36a92f18ea6f2bd3075a00
Aaron Brethorst <aaron@brethorsting.com>
2011-08-13 11:45:00 -0700

    prep for cookie handling

--- Orca/OrcaAPIClient.m
+++ Orca/OrcaAPIClient.m
@@ -42,4 +42,43 @@ NSString * const kOrcaBaseURLString = @"http://www.orcacard.com/";
     return [NSURL URLWithString:kOrcaBaseURLString];
 }
 
+- (NSMutableURLRequest *)requestWithMethod:(NSString *)method path:(NSString *)path parameters:(NSDictionary *)parameters
+{
+	NSMutableURLRequest *request = [[[NSMutableURLRequest alloc] init] autorelease];
+	NSMutableDictionary *headers = [NSMutableDictionary dictionaryWithDictionary:self.defaultHeaders];
+	NSURL *url = [NSURL URLWithString:path relativeToURL:[[self class] baseURL]];
+	
+    if (parameters)
+    {
+        NSMutableArray *mutableParameterComponents = [NSMutableArray array];
+    
+        for (id key in [parameters allKeys])
+        {
+            NSString *component = [NSString stringWithFormat:@"%@=%@", [key urlEncodedStringWithEncoding:kAFRestClientStringEncoding],
+                                                                       [[parameters valueForKey:key] urlEncodedStringWithEncoding:kAFRestClientStringEncoding]];
+            [mutableParameterComponents addObject:component];
+        }
+
+        NSString *queryString = [mutableParameterComponents componentsJoinedByString:@"&"];
+        
+        if ([method isEqualToString:@"GET"])
+        {
+            url = [NSURL URLWithString:[[url absoluteString] stringByAppendingFormat:[path rangeOfString:@"?"].location == NSNotFound ? @"?%@" : @"&%@", queryString]];
+        }
+        else
+        {
+            NSString *charset = (NSString *)CFStringConvertEncodingToIANACharSetName(CFStringConvertNSStringEncodingToEncoding(kAFRestClientStringEncoding));
+            [headers setObject:[NSString stringWithFormat:@"application/x-www-form-urlencoded; charset=%@", charset] forKey:@"Content-Type"];
+            [request setHTTPBody:[queryString dataUsingEncoding:NSUTF8StringEncoding]];
+        }
+    }
+    
+	[request setURL:url];
+	[request setHTTPMethod:method];
+	[request setHTTPShouldHandleCookies:NO];
+	[request setAllHTTPHeaderFields:headers];
+    
+	return request;
+}
+
 @end
